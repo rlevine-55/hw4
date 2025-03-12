aa55a287c406dc7be74b99653c2e96166a55cccf
Ryan Levine <ryan.levine@kellogg.northwestern.edu>
2025-03-12 02:52:58 +0000

    solved entries for users

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,16 +5,18 @@ class EntriesController < ApplicationController
   end
 
   def create
-    if User.find_by({ "id" => session["user_id"] }) != nil
+    @user = User.find_by({ "id" => session["user_id"] }) 
+    if @user != nil
     @entry = Entry.new
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
-    @entry["user_id"] = session["user_id"]
+    @entry["user_id"] = @user["id"]
     @entry.save
+    else
+   flash["notice"] = "Login first."
+    end
+      redirect_to "/places/#{@entry["place_id"]}"
   end
-    redirect_to "/places/#{@entry["place_id"]}"
-  end
-
 end
