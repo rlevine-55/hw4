6cb1685170fe4990bb8d8e55e3cf03d7808efffb
Ryan Levine <ryan.levine@kellogg.northwestern.edu>
2025-03-15 01:26:00 +0000

    image

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -9,6 +9,7 @@ class EntriesController < ApplicationController
     if @user != nil
     @entry = Entry.new
     @entry["title"] = params["title"]
+    @entry.uploaded_image.attach(params["uploaded_image"])
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
